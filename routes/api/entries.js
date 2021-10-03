d50a1d716e5d804dbc2ce57f59b4e8a7d6ed1063
ansarulSohan <ansarulsohan@gmail.com>
2021-10-03 14:18:11 +0600

    Modified entries

--- routes/api/entries.js
+++ routes/api/entries.js
@@ -1,49 +1,46 @@
 const { Entry, validateEntry } = require('../../models/Entry');
 const _ = require('lodash');
 const auth = require('../../middlewares/auth');
-
+const mongoDB = require('mongodb');
 const router = require('express').Router();
 
 
 router.get('/', auth, async (req, res) => {
-  let pageSize = req.params.pageSize || 10;
-  let pageNumber = req.params.pageNumber || 1
+  let pageSize = Number(req.query.pageSize) || 10;
+  let pageNumber = Number(req.query.pageNumber) || 1;
   let searchParameter = {};
   let sortParams = {};
-  req.params.order === 'desc' ? sortParams.order = req.params.sortBy : sortParams.order = 'asc';
-  req.params.sortBy ? sortParams.sortBy = req.params.sortBy : sortParams.sortBy = 'date';
+  req.query.order === 'desc' ? sortParams.order = req.query.sortBy : sortParams.order = 'asc';
+  req.query.sortBy ? sortParams.sortBy = req.query.sortBy : sortParams.sortBy = 'date';
 
   const sortObject = {};
   sortObject[sortParams.sortBy] = sortObject.order;
-
-  req.params.entryNumber ? searchParameter.entryNumber = {
-    $regex: req.query.entryNumber,
-    $option: 'i'
-  } : {};
-  req.params.author ? searchParameter.author = {
+  console.log(req.query.entryNumber);
+  req.query.entryNumber ? searchParameter.entryNumber = req.query.entryNumber : {};
+  req.query.author ? searchParameter.author = {
     $regex: req.query.author,
-    $option: 'i'
+    $options: 'i'
   } : {};
-  req.params.title ? searchParameter.title = {
+  req.query.title ? searchParameter.title = {
     $regex: req.query.title,
-    $option: 'i'
+    $options: 'i'
   } : {};
-  req.params.date ? searchParameter.date = {
+  req.query.date ? searchParameter.date = {
     $regex: req.query.date,
-    $option: 'i'
+    $options: 'i'
   } : {};
-  req.params.comments ? searchParameter.comments = {
+  req.query.comments ? searchParameter.comments = {
     $regex: req.query.comments,
-    $option: 'i'
+    $options: 'i'
   } : {};
 
   try {
     const count = await Entry.countDocuments({ ...searchParameter });
     const entries = await Entry.find({ ...searchParameter }).sort({ ...sortObject }).limit(pageSize).skip((pageNumber - 1) * pageSize);
     if (count < 1) {
-      res.status(404).send('Not found!');
+      return res.status(404).send('Not found!');
     }
-    res.status(200).json({
+    res.status(200).send({
       entries: entries,
       page: pageNumber,
       pageSize: pageSize,
@@ -52,7 +49,7 @@ router.get('/', auth, async (req, res) => {
     })
   } catch (error) {
     console.log(error);
-    res.send(400).json({
+    res.status(400).send({
       error: [
         {
           msg: error.message
@@ -62,6 +59,40 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+
+router.get('/:id', auth, async (req, res) => {
+  const isValidObjectId = mongoDB.ObjectId.isValid(req.params.id);
+  if (!isValidObjectId) {
+    return res.status(400).send({
+      error: [
+        {
+          msg: 'ObjectID is not valid'
+        }
+      ]
+    });
+  }
+  try {
+    const entry = await Entry.findById(req.params.id);
+    if (!entry) {
+      return res.status(404).send({
+        error: [
+          {
+            msg: 'Not found'
+          }
+        ]
+      });
+    }
+    res.status(200).send(entry);
+  } catch (error) {
+    res.status(400).send({
+      error: [
+        {
+          msg: error.message
+        }
+      ]
+    })
+  }
+})
 
 router.post('/', auth, async (req, res) => {
   try {
@@ -97,6 +128,7 @@ router.post('/', auth, async (req, res) => {
     });
   }
 });
+
 
 
 
