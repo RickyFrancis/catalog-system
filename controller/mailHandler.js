cef6889ffa0761faa01cca3a99146e7aa361fba8
ansarulSohan <ansarulsohan@gmail.com>
2021-10-09 15:27:37 +0600

    Chenged mail service to mail host, and tls unauthorized rejection at false

--- controller/mailHandler.js
+++ controller/mailHandler.js
@@ -2,9 +2,11 @@ const config = require('config')
 const nodemailer = require('nodemailer');
 const mailTemplate = require('../template/mailTemplate')
 
-module.exports = function (email, userName, code, url) {
+module.exports = function (email, userName, code, userId) {
+  let url = `${config.get('url')}/${userId}/${code}`
   const transporter = nodemailer.createTransport({
-    service: config.get('mailService'),
+    host: config.get('mailHost'),
+    tls: { rejectUnauthorized: false },
     auth: {
       user: config.get('mailAddress'),
       pass: config.get('mailPassword')
@@ -22,11 +24,12 @@ module.exports = function (email, userName, code, url) {
   transporter.sendMail(mailOption, (err, info) => {
     if (err) {
       isSuccessful = false;
+      console.log('[MAIL] Failed.')
     }
     else {
       isSuccessful = true;
       information = info;
-      console.log('Success');
+      console.log('[MAIL] Success');
     }
   });
   return { isSuccessful, information };
