b526a93b6b8ca1f930be9cce7086754074483b2a
ansarulSohan <ansarulsohan@gmail.com>
2021-10-09 16:15:48 +0600

    Added static react build file

--- server.js
+++ server.js
@@ -19,12 +19,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 //routes
-app.get('/', (req, res) => {
-  console.log('server is up and running');
-  res.send('Server is up and running');
-});
+// app.get('/', (req, res) => {
+//   console.log('server is up and running');
+//   res.send('Server is up and running');
+// });
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/entries', require('./routes/api/entries'));
